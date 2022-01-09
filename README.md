d5fb5bca5fb900d512fc8ec3202fc97aaf719c37
ZP <zairaperez01@gmail.com>
2022-01-08 21:04:15 -0600

    adding time function

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
 # finding-time
 
 #Purpose
-** A website which each employee is able to use to help them manage their time
-and keep them up to date. Its very easy to use this schedule. It will keep up with your
-tasks by informing you what is in the past and what is due next as it correlates with 
-the colors.
+** A website which each employee is able to use to help them manage their time and keep them up to date. Its very easy to use this schedule. It will keep up with your tasks by informing you what is in the past and what is due next as it correlates with 
+the colors. 
 
 # Build With
 ** HTML
@@ -16,6 +14,8 @@ the colors.
 **
 
 # Mock-up
+
+
 
 
 # Contribution
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,51 @@
 currentDay = document.getElementById("#currentDay");
+task = document.getElementById("#newtask");
+buttonbtn = document.querySelector("#button");
+hour = document.querySelector("time");
+textarea = document.querySelector("textarea");
+present = document.querySelector("#time");
+past = document.querySelector("#time");
+future = document.querySelector("#time");
 
-var currentDay = moment().format("dddd, MMMM Do"); 
- console.log(currentDay);   
- moment.currentDay()
+var newtask = []
+
+var currentDay = moment().format(); {
+$("#currentDay").text(moment().format("dddd, MMMM Do"));
+}
+
+
+
+var currentTask =  function () { 
+    var newtask= 
+        newtask = []
+        $("textarea#newtask").val(newtask); 
+        $(newtask).addClass(task);
+        $(task).append(textarea);
+        
+    }
+time = (moment().format("hour", 17)); 
+function time() {
+    if (hour < time) {
+    }
+    else if (hour == time) {
+    }
+    else if (hour > time) {
+    }
+    time();
+}
+
+
+        
+   savetask();
+
+
+function savetask(newtask) {
+JSON.parse(localStorage.getItem(newtask)); 
+
+}
+
+
+
+
+currentTask();
+buttonbtn.addEventListener("click", currentTask);
